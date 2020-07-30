9156018380c7d99111753c37b831c794e0c68ab2
jbarous <44842438+jbarous@users.noreply.github.com>
2020-07-30 14:11:57 +0200

    Update start.sh

--- ssh/centos7/start.sh
+++ ssh/centos7/start.sh
@@ -3,7 +3,7 @@
 __create_user() {
 # Create a user to SSH into as.
 useradd user
-SSH_USERPASS=newpass
+SSH_USERPASS=heslo
 echo -e "$SSH_USERPASS\n$SSH_USERPASS" | (passwd --stdin user)
 echo ssh user password: $SSH_USERPASS
 }
